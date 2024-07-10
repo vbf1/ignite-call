7535298e7ab6533a4703dc8281b92ecac97e5031
Victor Batista Ferreira <victor.vbf99@gmail.com>
2024-07-10 00:17:34 -0300

    feat: improvements

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,66 @@
+import { Adapter } from "next-auth/adapters";
+import { prisma } from "../prisma";
+
+/** @return { import("next-auth/adapters").Adapter } */
+export function PrismaAdapter(): Adapter {
+  return {
+    async createUser(user) {
+      return;
+    },
+    async getUser(id) {
+      const user = await prisma.user.findFirstOrThrow({ where: { id } });
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        avatar_url: user.avatar_url!,
+        emailVerified: null,
+      };
+    },
+    async getUserByEmail(email) {
+      const user = await prisma.user.findFirstOrThrow({ where: { email } });
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        avatar_url: user.avatar_url!,
+        emailVerified: null,
+      };
+    },
+    async getUserByAccount({ providerAccountId, provider }) {
+      return;
+    },
+    async updateUser(user) {
+      return;
+    },
+    async deleteUser(userId) {
+      return;
+    },
+    async linkAccount(account) {
+      return;
+    },
+    async unlinkAccount({ providerAccountId, provider }) {
+      return;
+    },
+    async createSession({ sessionToken, userId, expires }) {
+      return;
+    },
+    async getSessionAndUser(sessionToken) {
+      return;
+    },
+    async updateSession({ sessionToken }) {
+      return;
+    },
+    async deleteSession(sessionToken) {
+      return;
+    },
+    async createVerificationToken({ identifier, expires, token }) {
+      return;
+    },
+    async useVerificationToken({ identifier, token }) {
+      return;
+    },
+  };
+}
